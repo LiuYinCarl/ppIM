45e0e62784d7a01781989f31c8c4f0d6786db7d5
Kenshin <1427518212@qq.com>
2022-04-26 16:33:45 +0800

    实现消息加密

--- network.py
+++ network.py
@@ -3,41 +3,78 @@
 
 import logging
 import unittest
+import random
+from Crypto.Cipher import AES
+import base64
 
-MAX_PACK_SIZE = (1 << 17) - 1
+PACKET_HEAD_LENGTH = 2
+MAX_PACK_SIZE = (1 << 16) - 1
+AES_SECRET_KEY = b"ufahdufhaiufugil"
 
 class NetPacketMgr(object):
     def __init__(self):
         self.msg = []
         self.msgpond = b""
         self.msg_len = 0
+        self.aes = AES.new(AES_SECRET_KEY, AES.MODE_ECB)
 
-    def get_packet_head_len(self, packet_head):
-        return int.from_bytes(packet_head, byteorder="big")
-
-    def make_packet_head(self, packet_body_len):
+    def pack_header(self, packet_body_len) -> bytes:
         if packet_body_len > MAX_PACK_SIZE:
             return False, None
-
-        head = packet_body_len.to_bytes(4, byteorder="big")
+        head = packet_body_len.to_bytes(PACKET_HEAD_LENGTH, byteorder="big")
         return True, head
+    
+    def unpack_header(self, packet_header) -> int:
+        return int.from_bytes(packet_header, byteorder="big")
 
-    def pack_msg(self, data):
-        bdata = data.encode(encoding="utf8")
-        str_len = str(len(bdata))
-        return self
+    def pack_body(self, data:str) -> bytes:
+        return bytes(data, encoding="utf8")
 
+    def unpack_body(self, packet_body:bytes) -> str:
+        return str(packet_body, encoding="utf8")
+
+    def encrypt(self, data:str) -> str:
+        data = data.encode("utf8")
+        while len(data) % 16 != 0:
+            data += b"\x00"
+        en_data = self.aes.encrypt(data)
+        return str(base64.b64encode(en_data), encoding="utf8")
+
+    def decrypt(self, data:str) -> str:
+        data = data.encode("utf8")
+        data = base64.b64decode(data)
+        de_data = self.aes.decrypt(data)
+        return str(de_data, encoding="utf8").strip('\0')
 
 class TestNetPacketMgr(unittest.TestCase):
-    def test_pack_head(self):
+    def get_rand_str(self):
+        s = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+        res = [random.choice(s) for i in range(100)]
+        return "".join(res)
+
+    def test_pack_header(self):
         netpack_mgr = NetPacketMgr()
         for packet_len in range(1, MAX_PACK_SIZE):
-            ok, head = netpack_mgr.make_packet_head(packet_len)
-            if ok == False:
-                logging.error("pack head failed.")
-                return
-            new_packet_len = netpack_mgr.get_packet_head_len(head)
-            assert packet_len == new_packet_len, "packet_len:{} new_packet_len:{}".format(packet_len, new_n)
+            ok, head = netpack_mgr.pack_header(packet_len)
+            self.assertTrue(ok)
+            new_packet_len = netpack_mgr.unpack_header(head)
+            self.assertEqual(packet_len, new_packet_len)
+    
+    def test_pack_body(self):
+        netpack_mgr = NetPacketMgr()
+        for i in range(100):
+            data = self.get_rand_str()
+            packet_body = netpack_mgr.pack_body(data)
+            new_data = netpack_mgr.unpack_body(packet_body)
+            self.assertEqual(data, new_data)
+
+    def test_encrypt(self):
+        netpack_mgr = NetPacketMgr()
+        for i in range(100):
+            data = self.get_rand_str()
+            packet_body = netpack_mgr.encrypt(data)
+            new_data = netpack_mgr.decrypt(packet_body)
+            self.assertEqual(data, new_data)
 
 
 if __name__ == "__main__":
